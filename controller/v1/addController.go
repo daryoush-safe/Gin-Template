9f7f55c5644224cfb3580144b5239f4462e56e48
daryoush <daryoosh.safe@gmail.com>
2024-09-05 00:00:43 +0330

    feat: add validation

--- controller/v1/addController.go
+++ controller/v1/addController.go
@@ -3,22 +3,37 @@ package controller_v1
 import (
 	"first-project/application"
 	"fmt"
+
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 
 	"net/http"
 )
 
+type AddParams struct {
+	Num1 string `uri:"num1" validate:"required,numeric"`
+	Num2 string `uri:"num2" validate:"required,numeric"`
+}
+
 func Add(c *gin.Context) {
-	num1, e := strconv.Atoi(c.Param("num1"))
-	if e != nil {
-		fmt.Println("invalid num1")
+	var validate *validator.Validate = validator.New()
+	// validate = validator.New()
+	var params AddParams
+
+	if err := c.ShouldBindUri(&params); err != nil {
+		fmt.Print("ridi")
+		return
 	}
-	num2, e := strconv.Atoi(c.Param("num2"))
-	if e != nil {
-		fmt.Println("invalid num2")
+	if err := validate.Struct(params); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Parameters must be numeric"})
+		return
 	}
+
+	num1, _ := strconv.Atoi(c.Param("num1"))
+	num2, _ := strconv.Atoi(c.Param("num2"))
+
 	sum := application.Add(num1, num2)
 	c.String(http.StatusOK, strconv.Itoa(sum))
 }
