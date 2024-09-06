eb9811f96f593750ec1f85f0c0a085b2b80f7ba2
koorosh <koorosh>
2024-09-06 17:52:23 +0330

    chore: create standard response method

--- src/controller/response.go
+++ src/controller/response.go
@@ -1,12 +1,44 @@
 package controller
 
-type Response struct {
-	Status  int16  `json:"statusCode"`
-	Message string `json:"message"`
-	Data    any    `json:"data"`
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type singleMessageResponse struct {
+	Status  int         `json:"statusCode"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
-type ValidationResponse struct {
-	Status   int16             `json:"statusCode"`
+type multipleMessageResponse struct {
+	Status   int               `json:"statusCode"`
 	Messages map[string]string `json:"messages"`
+	Data     interface{}       `json:"data"`
+}
+
+func Response[T string | map[string]string](c *gin.Context, statusCode int, message T, data interface{}) {
+	statusText := http.StatusText(statusCode)
+	if statusText == "" {
+		panic("invalid status code!")
+	}
+
+	switch msg := any(message).(type) {
+	case string:
+		if msg == "" {
+			msg = statusText
+		}
+		c.JSON(statusCode, singleMessageResponse{
+			Status:  statusCode,
+			Message: msg,
+			Data:    data,
+		})
+	case map[string]string:
+		c.JSON(statusCode, multipleMessageResponse{
+			Status:   statusCode,
+			Messages: msg,
+			Data:     data,
+		})
+	}
 }
